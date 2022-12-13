4309ea428ad12482c3dff3e4fc2d57197c83493e
BikmanDjuma <bikmangeek@gmail.com>
2022-12-13 03:54:37 -0800

    new staff added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <h1>{{title}}-{{names}}</h1>
+    <input type="text" ref="name">
+    <button @click="handleClick">Click me</button>
+    <Modal header="Sing up here !" text="creating youtr account here "/>
   </div>
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
+
 export default {
-  name: 'App'
+
+  name: 'App',
+
+  components : { Modal },
+
+  data() {
+    return {
+      title: "Testing !",
+      names: "Bikman",
+    }
+  },
+
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name),
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }
+
 }
 </script>
 
@@ -19,5 +42,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1{
+  color:red;
+  border-bottom:1px solid #ddd;
+  display:inline-block;
+  padding:10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="backdrop">
+		<div class="modal">
+			<h1>{{ header }}</h1>
+			<p>{{ text }}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+	
+	export default {
+		props:['header','text']
+	}
+
+</script>
+
+<style scoped>
+
+	.modal{
+		width:400px;
+		padding:20px;
+		margin:100px auto;
+		background-color:white;
+		border-radius:10px;
+	}
+
+	.backdrop{
+		top:0px;
+		position:fixed;
+		background:rgb(0,0,0,0.5);
+		width:100%;
+		height:100%;
+	}
+	h1{
+		color:#03cfb4;
+		padding:0px;
+		border:none;
+	}
+</style>
